f34b17df41e14f873020581fbef0a583e3ccc1be
Asish1992-coder <asish.kar1992@gmail.com>
2026-09-06 09:38:35 +0530

    Add Remote State file

--- main.tf
+++ main.tf
@@ -5,8 +5,30 @@ terraform {
       version = "~> 3.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate-backend"
+    storage_account_name = "tsstatestorage1992"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+    use_azuread_auth     = true
+  }
 }
+
+
 
 provider "azurerm" {
   features {}
+}
+
+resource "azurerm_resource_group" "lab"{
+    name = "rg-terraform-lab-dev"
+    location = "Central India"
+}
+resource "azurerm_storage_account" "lab" {
+  resource_group_name = azurerm_resource_group.lab.name
+  location = azurerm_resource_group.lab.location
+  name = "sttflabdev1992"
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+  
 }
